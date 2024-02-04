b33f2d4f3397a048cc5fdac61a23752c60119288
iskander-d <drozenko21@gmail.com>
2024-02-04 19:56:15 +0400

    changeConfig

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -14,6 +14,7 @@ import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
 public class Attach {
+
     @Attachment(value = "{attachName}", type = "image/png")
     public static byte[] screenshotAs(String attachName) {
         return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
@@ -44,7 +45,8 @@ public class Attach {
     }
 
     public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
+        String selenoidBaseUrl = System.getProperty("selenoidBaseUrl", "selenoid.autotests.cloud");
+        String videoUrl = "https://" + selenoidBaseUrl + "/video/" + sessionId() + ".mp4";
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -3,56 +3,48 @@ package test;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import config.WebDriverConfig;
-
+import config.ConfigReader;
+import config.WebConfig;
+import config.WebConfigForProject;
 
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.junit.jupiter.api.BeforeEach;
 import pages.MainPage;
 import pages.NavigationPanel;
 
-
+import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 public class TestBase {
 
     MainPage mainPage = new MainPage();
     NavigationPanel navigationPanel = new NavigationPanel();
-    static WebDriverConfig config = ConfigFactory.create(WebDriverConfig.class, System.getProperties());
+    private static final WebConfig config = ConfigReader.Instance.read();
 
-    @BeforeAll
-    public static void setUp() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        Configuration.baseUrl = config.getBaseUrl();
-        Configuration.browser = config.getBrowserName();
-        Configuration.browserVersion = config.getBrowserVersion();
-        Configuration.browserSize = config.getBrowserSize();
+        @BeforeAll
+        public static void beforeAll() {
+            WebConfigForProject webConfigForProject = new WebConfigForProject(config);
+            webConfigForProject.webConfig();
 
-        if (config.isRemote()) {
-            Configuration.remote = config.getRemoteUrl();
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
         }
-    }
 
-    @AfterEach
-    void afterTestsRemote() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
+        @BeforeEach
+        void addListener() {
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        }
 
-        if (config.isRemote()) {
+        @AfterEach
+        void addAttachments() {
+            Attach.screenshotAs("Last screenshot");
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
             Attach.addVideo();
-        }
-        Selenide.closeWebDriver();
-    }
 
-}
+            closeWebDriver();
+        }
+    }
